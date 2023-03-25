5fa5a40bcd3b23314cdd38a396e03ec0d7f6394c
jacobaranza <jacobthomasjunior@gmail.com>
2023-03-25 11:27:24 -0500

    function parameters lab

---
+++ index.js
@@ -0,0 +1,4 @@
+function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+  }
+  console.log(introductionWithLanguage("Jacob", "JavaScript"));
