96800fe1a8d3b1cc9d19f4f7690965805a701a42
jacobaranza <jacobthomasjunior@gmail.com>
2023-03-25 11:36:04 -0500

    function parametes lab

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function introductionWithLanguage(name, language) {
+function introductionWithLanguageOptional(name, language) {
     return `Hi, my name is ${name} and I am learning to program in ${language}.`;
   }
-  console.log(introductionWithLanguage("Jacob", "JavaScript"));
+  console.log(introductionWithLanguageOptional("name", "JavaScript"));
